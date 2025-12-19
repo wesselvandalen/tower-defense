1a96f5c93f2d6a2cc40394d1e7114b823bf54073
RipInPease <svorrevfjelde@gmail.com>
2025-12-19 21:48:57 +0100

    set path test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 mod map;
 mod towers;
+
 use towers::*;
+use map::Map;
 
 fn main() {
+    let mut map = Map::new();
 
+    let path = vec![
+        (1, 1),
+        (2, 2)
+    ];
+    map.set_path(path);
 
     println!("Hello, world!");
 }
